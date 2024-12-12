d6dd7b69d2ee52ecea05edf7d1a7932dc02b173f
levyxx <rb.06p.8046@s.thers.ac.jp>
2024-12-13 04:22:35 +0900

    d->D

--- command.go
+++ command.go
@@ -29,7 +29,7 @@ func onHelpCommand(aSession *discordgo.Session, aInteraction *discordgo.Interact
 		Title: "機能説明",
 		Description: "任意のn面ダイスを複数個同時に振ることができます\n\n" +
 			"**__各種コマンド__**\n\n" +
-			"**random**\n`/random mdn`でn面ダイスをm個振ります\n" +
+			"**random**\n`/random mDn`でn面ダイスをm個振ります\n" +
 			"**help**\nヘルプを表示します",
 		Footer: &tFooter,
 	}
